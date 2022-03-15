8773852dec06a9f86d8e5c25706ac5e309cd8479
danilosmod <danilo.oldschool@gmail.com>
2022-03-15 11:41:53 -0300

    função cria array com 5 numero sorteados

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ function sorteiaNumeroPokemon(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(sorteiaNumeroPokemon(1, 151));
+function criaArrayNumeroPokemon() {
+    let arrayNumerosPokemon = [];
+    for (i = 0; i < 5; i++) {
+        arrayNumerosPokemon.push(sorteiaNumeroPokemon(1, 151));
+    }
+    return arrayNumerosPokemon;
+}
+
