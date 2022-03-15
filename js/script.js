15786a06df63848ebd31e6c8904189fcc93ae998
danilosmod <danilo.oldschool@gmail.com>
2022-03-15 11:51:29 -0300

    Função para sotear numero de definir a raridade baseado em porcentagem

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ function sorteiaNumeroPokemon(min, max) {
 }
 
 function criaArrayNumeroPokemon() {
-    let arrayNumerosPokemon = [];
+    const arrayNumerosPokemon = [];
     for (i = 0; i < 5; i++) {
         arrayNumerosPokemon.push(sorteiaNumeroPokemon(1, 151));
     }
     return arrayNumerosPokemon;
 }
 
+console.log(criaArrayNumeroPokemon());
+
+function sorteiaRaridade(min, max) {
+    const numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+    let raridade = '';
+    if (numeroSorteado >= 1 && numeroSorteado <= 25) {
+        raridade = 'lendario';
+    } else if (numeroSorteado > 25 && numeroSorteado < 50) {
+        raridade = 'normal';
+    } else if (numeroSorteado >= 50 && numeroSorteado < 75) {
+        raridade = 'raro';
+    } else raridade = 'epico';
+    return raridade;
+}
+
+console.log(sorteiaRaridade(1, 100));
