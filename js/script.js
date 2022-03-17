2c2566162c06e8a7c17f796478ed152b545c1cc0
danilosmod <danilo.oldschool@gmail.com>
2022-03-17 10:04:33 -0300

    Criação de estrutura HTML

--- js/script.js
+++ js/script.js
@@ -1,51 +1,52 @@
 const pokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-function sorteiaNumeroPokemon(min, max) {
+function rafflePokemonNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function listaNumeroPokemon() {
+function pokemonNumberList() {
     const arrayNumerosPokemon = [];
     for (i = 0; i < 5; i++) {
-        arrayNumerosPokemon.push(sorteiaNumeroPokemon(1, 151));
+        arrayNumerosPokemon.push(rafflePokemonNumber(1, 151));
     }
     return arrayNumerosPokemon;
 }
 
-console.log(listaNumeroPokemon());
+console.log(pokemonNumberList());
 
-function sorteiaRaridade(min, max) {
+function defineRarity(min, max) {
     const numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
-    let raridade = '';
+    let rarity = '';
     if (numeroSorteado == 1) {
-        raridade = 'lendario';
+        rarity = 'lendario';
     } else if (numeroSorteado > 1 && numeroSorteado <= 11) {
-        raridade = 'epico';
+        rarity = 'epico';
     } else if (numeroSorteado > 11 && numeroSorteado <= 42) {
-        raridade = 'raro';
-    } else raridade = 'normal';
-    return raridade;
+        rarity = 'raro';
+    } else rarity = 'normal';
+    return rarity;
 }
 
-console.log(sorteiaRaridade(1, 100));
+console.log(defineRarity(1, 100));
 
-function listaRaridadePokemon() {
-    const arrayRaridadePokemon = [];
+function pokemonRarityList() {
+    const pokemonRarityArray = [];
     for (i = 0; i < 5; i++) {
-        arrayRaridadePokemon.push(sorteiaRaridade(1, 100));
+        pokemonRarityArray.push(defineRarity(1, 100));
     }
-    return arrayRaridadePokemon;
+    return pokemonRarityArray;
 }
 
-console.log(listaRaridadePokemon());
+console.log(pokemonRarityList());
 
-function listaPokemons() {
-    const numeroPokemon = listaNumeroPokemon();
-    const nomePokemon = [];
+function pokemonList() {
+    const pokemonNumber = pokemonNumberList();
+    const pokemonName = [];
     for (i = 0; i < 5; i++) {
-        nomePokemon.push(fetch(pokemonUrl(numeroPokemon[i])).then(response => response.json()).then(pokemon => { return pokemon.name }));
+        pokemonName.push(fetch(pokemonUrl(pokemonNumber[i])).then(response => response.json()).then(pokemon => { return pokemon.name }));
     }
-    return nomePokemon;
+    return pokemonName;
 }
 
-console.log(listaPokemons());
+console.log(pokemonList());
+
